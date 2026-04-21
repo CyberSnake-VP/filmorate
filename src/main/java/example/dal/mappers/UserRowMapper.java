b06e6568be0fd1a21bb8844b68a338b561f38501
Vadim <zigmundfreid89@yandex.ru>
2026-04-21 13:48:11 +0300

    Создан UserRowMapper

--- src/main/java/example/dal/mappers/UserRowMapper.java
+++ src/main/java/example/dal/mappers/UserRowMapper.java
@@ -5,6 +5,7 @@ import example.model.User;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -13,6 +14,15 @@ public class UserRowMapper implements RowMapper<User> {
 
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+        User user = new User();
+        user.setId(rs.getLong("id"));
+        user.setEmail(rs.getString("email"));
+        user.setLogin(rs.getString("login"));
+
+        Date birthday = rs.getDate("birthday");
+        if(birthday != null) {
+            user.setBirthday(birthday.toLocalDate());
+        }
+        return user;
     }
 }
---
+++ src/main/java/example/service/UserService.java
@@ -0,0 +1,17 @@
+package example.service;
+
+
+import example.dto.request.CreateUserRequest;
+import example.dto.request.UpdateUserRequest;
+import example.dto.response.UserResponse;
+import example.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponse create(CreateUserRequest request);
+    UserResponse get(Long id);
+    List<User> getAll();
+    UserResponse update(UpdateUserRequest request, Long id);
+    void delete(Long id);
+}
