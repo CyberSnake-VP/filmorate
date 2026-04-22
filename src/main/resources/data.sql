02c89e20b5d8d5f747f6b7ce6fcc9792a6ddec66
Vadim <zigmundfreid89@yandex.ru>
2026-04-22 12:56:33 +0300

    Заполнить таблицы mpa_rating, genres

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+-- категории фильмов
+-- такой merge работает только h2 в postgresql строже обязательно
+MERGE INTO genres (genre_id, genre_name) VALUES (1, 'Комедия');
+MERGE INTO genres (genre_id, genre_name) VALUES (2, 'Драма');
+MERGE INTO genres (genre_id, genre_name) VALUES (3, 'Мультфильмы');
+MERGE INTO genres (genre_id, genre_name) VALUES ( 4, 'Биография');
+MERGE INTO genres (genre_id, genre_name) VALUES ( 5, 'Триллер');
+MERGE INTO genres (genre_id, genre_name) VALUES (6, 'Документальный');
+MERGE INTO genres (genre_id, genre_name) VALUES (7, 'Боевик');
+
+MERGE INTO mpa_rating (mpa_id, mpa_name) VALUES (1, 'G');
+MERGE INTO mpa_rating (mpa_id, mpa_name) VALUES (2, 'PG');
+MERGE INTO mpa_rating (mpa_id, mpa_name) VALUES (3, 'PG-13');
+MERGE INTO mpa_rating (mpa_id, mpa_name) VALUES (4, 'R');
+MERGE INTO mpa_rating (mpa_id, mpa_name) VALUES (5, 'NC-17');
