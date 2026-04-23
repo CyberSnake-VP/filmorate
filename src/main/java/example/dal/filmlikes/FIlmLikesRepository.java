7749dbc01e379e40ae7ad2b11ac7dca484d25654
Vadim <zigmundfreid89@yandex.ru>
2026-04-23 22:06:18 +0300

    Создать FilmLikesRepository

--- src/main/java/example/dal/filmlikes/FIlmLikesRepository.java
+++ src/main/java/example/dal/filmlikes/FIlmLikesRepository.java
@@ -1,5 +1,9 @@
 package example.dal.filmlikes;
 
-public interface FIlmLikesRepository {
+import java.util.List;
 
+public interface FIlmLikesRepository {
+    void addLike(Long filmId, Long userId);
+    void removeLike(Long filmId, Long userId);
+    List<Long> getLikes(Long filmId);
 }
--- src/main/java/example/dal/filmlikes/FilmLikesRepositoryImpl.java
+++ src/main/java/example/dal/filmlikes/FilmLikesRepositoryImpl.java
@@ -1,8 +1,43 @@
 package example.dal.filmlikes;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class FilmLikesRepositoryImpl implements FIlmLikesRepository{
+import java.util.Collections;
+import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
+public class FilmLikesRepositoryImpl implements FIlmLikesRepository {
+
+    private static final String ADD_LIKE_QUERY = "INSERT INTO film_likes (film_id, user_id) VALUES (?, ?)";
+    private static final String REMOVE_LIKE_QUERY = "DELETE FROM film_likes WHERE film_id = ? AND user_id = ?";
+    private static final String GET_LIKES_QUERY = "SELECT user_id FROM film_likes WHERE film_id = ?";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void addLike(Long filmId, Long userId) {
+        if (filmId != null && userId != null) {
+            jdbcTemplate.update(ADD_LIKE_QUERY, filmId, userId);
+        }
+    }
+
+    @Override
+    public void removeLike(Long filmId, Long userId) {
+        if (filmId != null && userId != null) {
+            jdbcTemplate.update(REMOVE_LIKE_QUERY, filmId, userId);
+        }
+    }
+
+    @Override
+    public List<Long> getLikes(Long filmId) {
+        if(filmId == null) {
+            return Collections.emptyList();
+        }
+
+        return jdbcTemplate.query(GET_LIKES_QUERY,
+                (rs, rowNum) -> rs.getLong("user_id"), filmId);
+    }
 }
